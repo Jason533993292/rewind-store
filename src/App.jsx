192b4612b175afcfac9f389e570252d0b82bbac5
Philzlzy <your-email@example.com>
2026-06-28 10:28:26 +0200

    V6.3.0

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const TWEAK_DEFAULTS = {
   showStock: true,
 };
 
-const VERSION = 'V6.2.11';
+const VERSION = 'V6.3.0';
 
 export default function App() {
   const [t, setTweak] = useTweaks(TWEAK_DEFAULTS);
